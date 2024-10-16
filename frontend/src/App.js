bce915c95ad4212e6ed65b3015d4d68840a98d5b
moein shahcheraghi <moeinshah0@gmail.com>
2024-10-16 11:27:26 +0330

    change code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,22 @@
 // frontend/src/App.js
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
-import Admin from './pages/Admin';
-import Navbar from './components/Navbar';
+// سایر کامپوننت‌ها
 
 function App() {
-    return (
-        <Router>
-            <Navbar />
-            <Switch>
-                <Route path="/" exact component={Home} />
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-                <Route path="/admin" component={Admin} />
-            </Switch>
-        </Router>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        {/* سایر مسیرها */}
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
